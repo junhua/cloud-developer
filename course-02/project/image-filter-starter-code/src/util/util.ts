9ef2fd7e067a66cc0cf4626052158c3dd161042a
Liu Junhua <junhua.viva@gmail.com>
2019-12-15 11:22:14 +0800

    updated util

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -19,7 +19,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
         .write(__dirname+outpath, (img)=>{
             resolve(__dirname+outpath);
         });
-    });
+    }).catch(error => { console.log('Oops', error.message); });
 }
 
 // deleteLocalFiles
